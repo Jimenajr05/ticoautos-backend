bd5d0d8e592b1ac91926879ec8d071d7073960cf
Jimena <mariajimenajr14@gmail.com>
2026-03-06 16:39:20 -0600

    Configuración de CORS en la aplicación Express

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const vehicleRoutes = require('./routes/vehicle.routes');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/auth', authRoutes);
