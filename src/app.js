38b7dc40e88f416000466fc521418cb251043e8c
Jimena <mariajimenajr14@gmail.com>
2026-03-09 15:07:49 -0600

    Configuración de servidor para servir archivos de imágenes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const authRoutes = require('./routes/authRoutes');
 const vehicleRoutes = require('./routes/vehicle.routes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
 app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/auth', authRoutes);
 
